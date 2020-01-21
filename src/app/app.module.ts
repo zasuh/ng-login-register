40e190105e49f567ca15fde27a2f5258c163a5e9
Zane Suhadolnik <zanesuhi@gmail.com>
2020-01-21 06:18:57 +0100

    Added error and jwt interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { fakeBackendProvider } from './_helpers/fake-backend';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './_components/register/register.component';
@@ -25,6 +26,8 @@ import { LoginComponent } from './_components/login/login.component';
     HttpClientModule
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     fakeBackendProvider,
   ],
   bootstrap: [AppComponent],
